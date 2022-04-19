4763eafb23104b99ffe2e08cdcf078a51d20f35b
Evgen <evgenrus257@yandex.ru>
2022-04-20 02:06:30 +0700

    OrderDbContext created

---
+++ Orders/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.Database;
+
+namespace Orders.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class OrderController : Controller
+{
+    private OrderDbContext _context { get; set; }
+
+    public OrderController(OrderDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddItemToCart(int productid, int count = 1)
+    {
+        
+    }
+}
---
+++ Orders/Service/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using Orders.Database.Entities;
+
+namespace Orders.Service;
+
+public interface IOrderService
+{
+    public Task<ICollection<Item>> GetAllItems();
+    public Task<ICollection<Item>> GetItemById(int id);
+    public Task AddItemToCart(int cartId, Item item);
+}
