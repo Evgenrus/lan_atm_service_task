92d1b05972fcc2d9923d007ec870f41db34b01df
Evgen <evgenrus257@yandex.ru>
2022-05-05 13:41:07 +0700

    1

---
+++ Orders/Database/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Orders.Database.Entities;
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public List<Order> Orders { get; set; } = new List<Order>();
+}
