b826f0d9af13d634d57d92aa323f431e473c9329
Evgen <evgenrus257@yandex.ru>
2022-05-15 20:40:55 +0700

    Fixed merge conflict

--- Delivery/Program.cs
+++ Delivery/Program.cs
@@ -1,3 +1,7 @@
+using Delivery.Data;
+using Delivery.Data.Entities;
+using Delivery.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DeliveryDbContext>();
+builder.Services.AddScoped<IDeliveryService, DeliveryService>();
 
 var app = builder.Build();
 
