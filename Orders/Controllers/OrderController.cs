dd159e74c3a01a432b7d6a07168b1e6d76faef87
Evgen <evgenrus257@yandex.ru>
2022-05-16 11:47:00 +0700

    Database updated

--- Orders/Controllers/OrderController.cs
+++ Orders/Controllers/OrderController.cs
@@ -117,23 +117,5 @@ public class OrderController : ControllerBase
 
         return Ok();
     }
-    
-[Route("api/v1/[controller]")]
-public class OrderController : Controller
-{
-    private OrderDbContext _context { get; set; }
 
-    public OrderController(OrderDbContext context)
-    {
-        _context = context;
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> AddItemToCart(int productid, int cartId, int count = 1)
-    {
-        if (count < 1)
-            return BadRequest("Count should be equal or greater then 1");
-        if (productid < 1 || cartId < 1)
-            return BadRequest("Wrong ID");
-    }
 }
